e325bd6d85e530a7fb95c9f35aaf3af679a05667
Daniel Choudhury <dannychoudhury@gmail.com>
2024-02-12 16:15:50 +0700

    Clean up supabase middleware to use authDecoder by passing full cookie in

--- supabase/web/src/entry.server.tsx
+++ supabase/web/src/entry.server.tsx
@@ -1,7 +1,6 @@
-import { createServerClient } from '@supabase/ssr'
-import cookie from 'cookie'
 
 import { authDecoder } from '@redwoodjs/auth-supabase-api'
+import { MiddlewareRequest, MiddlewareResponse } from '@redwoodjs/vite/middleware'
 
 import App from './App'
 import { Document } from './Document'
@@ -13,72 +12,52 @@ interface Props {
   meta?: any[]
 }
 
-export const middleware = async (req: Request) => {
+export const middleware = async (req: MiddlewareRequest) => {
+  const mwRes = MiddlewareResponse.next()
+
   const cookieHeader = req.headers.get('Cookie')
-  console.log(`👉 \n ~ file: entry.server.tsx:17 ~ cookieHeader:`, cookieHeader)
 
+  // Unauthenticated request
   if (!cookieHeader) {
-    return {
-      context: {
-        loading: false,
-        isAuthenticated: false,
-        userMetadata: null,
-        currentUser: null,
-        hasError: false,
-      },
-    }
+    return null
   }
 
-  const parsedCookie = cookie.parse(cookieHeader)
-  // @MARK: problem here, is that we don't know the name of the cookie
-  // so we have to use the supabase server client to get the cookie 🤷
-  console.log(`👉 \n ~ file: entry.server.tsx:31 ~ parsedCookie:`, parsedCookie)
+  // @NOTE to maintain compatibility with the api side,
+  // we need to pass the whole cookieString to the authDecoder
+  // This is because we don't know what the name of the cookie is!
+  const cookieString = req.headers.get('cookie')
 
-  const supabase = createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_KEY!,
-    {
-      cookies: {
-        get(key) {
-          return parsedCookie[key]
-        },
-      },
-    }
-  )
+  // @WARN: Authdecoders still take event and context
+  // @TODO: is the decodedSession the same as jwt.decode?
+  try {
+    const decodedSession = await authDecoder(cookieString, 'supabase', {
+      event: {} as any,
+      context: {} as any,
+    })
+  } catch (e) {
+    // @TODO, @MARK Unsure what to do if authDecoding fails.
+    // unlike firebase, and dbauth - where we unset the cookie, supabase itself sets these cookies
+    console.error('Failed to decode session in middleware')
+  }
 
-  // @MARK: we're getting the user from supabase ssr auth directly
-  // Should we be using the authDecoder and getCurrentUser functions to maintain consistency?
-  const user = await supabase.auth.getUser()
-  const seshun = await supabase.auth.getSession()
-  console.log(`👉 \n ~ file: entry.server.tsx:52 ~ seshun:`, seshun)
-  console.log(`👉 \n ~ file: entry.server.tsx:51 ~ user:`, user)
 
-  // @TODO: why is the type definition requesting req? It's not used.
-  // See definition of supabase authDecoder
-  // const decodedSession = authDecoder(accessToken, 'supabase', {
-  //   event: {},
-  //   context: {},
-  // })
-
-  // @TODO: getCurrentUser in template destructures event and context
+  // @MARK @WARN getCurrentUser in template destructures event and context
   // this is problematic, as these values are now different between api and web server.
-  // const currentUser = await getCurrentUser(decodedSession, {
-  //   token: accessToken,
-  //   type: 'supabase',
-  // })
+  const currentUser = await getCurrentUser(decodedSession, {
+    token: cookieString,
+    type: 'supabase',
+  })
 
-  // console.log(`👉 \n ~ file: entry.server.tsx:29 ~ currentUser:`, currentUser)
-
-  return {
-    context: {
-      currentUser: user.data.user,
+  req.serverAuthContext.set({
+    currentUser,
       loading: false,
-      isAuthenticated: !!user.data.user,
+    isAuthenticated: !!currentUser,
       hasError: false,
-      userMetadata: user.data.user.user_metadata, // where does this come from?
-      encryptedSession: seshun.data.session.access_token,
-    },
-  }
+    userMetadata: currentUser.user_metadata, // where does this come from?
+    cookieHeader: cookieString,
+  })
+
+  return mwRes
 }
 
 export const ServerEntry: React.FC<Props> = ({ css, meta }) => {
--- supabase/web/src/pages/LoginPage/LoginPage.tsx
+++ supabase/web/src/pages/LoginPage/LoginPage.tsx
@@ -63,7 +63,7 @@ const LoginPage = () => {
                     className="rw-label"
                     errorClassName="rw-label rw-label-error"
                   >
-                    Username
+                    Email
                   </Label>
                   <TextField
                     name="username"
